2767ee92810a9db0e5296efca3a7f523fdcdaafa
Altrixity <altrixityyt@gmail.com>
2026-04-29 10:46:25 -0400

    OLDEST VERSION FOUND AS OF APR 29th

---
+++ Redirect.js
@@ -0,0 +1,9 @@
+    // catch enter code in search form in front page
+    $('#search').keypress(function (e) {
+        var str = $('#search').val();
+        var domain = "http://www.yourdomain.com";
+        var url = domain+"default.aspx?search=" + str;
+        if (e.keyCode == 13) {
+            location.href = url;
+        }
+    });
